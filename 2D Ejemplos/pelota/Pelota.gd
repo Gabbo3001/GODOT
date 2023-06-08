5425605eff88b2f83cae909817843f189fce6530
Juan Gabriel Bustinza <gabrielbustinza1995@gmail.com>
2023-06-07 23:54:36 -0500

    update 7/06

--- 2D Ejemplos/pelota/Pelota.gd
+++ 2D Ejemplos/pelota/Pelota.gd
@@ -1,16 +1,22 @@
 extends RigidBody2D
-
+var game_started : bool = false
 func _input(event):
-	if event.is_action_pressed("iniciar"):
-		linear_velocity = Vector2(50,-200)
-		
+	if game_started == false:
+		if event.is_action_pressed("iniciar"):
+			linear_velocity = Vector2(50,-200)
+			game_started = true
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	for body in get_colliding_bodies():
+		if body.is_in_group("bloques"):
+			body.queue_free()
+			if body.get_parent().get_child_count() == 1:
+				print("GANA")
+				queue_free()
+		elif body.get_name() == "borde_bot":
+			print("PIERDE")
+			queue_free()
+	pass
